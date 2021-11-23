a26bbb6607132a254813a4a2f059c5affecd6382
m.zaky <m.zaky@hukumonline.com>
2021-11-23 11:25:09 +0700

    add folder build storybook to gitignore

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//   "stories": [
-//     "../src/**/*.stories.mdx",
-//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
-//   ],
-//   "addons": [
-//     "@storybook/addon-links",
-//     "@storybook/addon-essentials",
-//     "@storybook/preset-create-react-app"
-//   ]
-// }
-
-
 const path = require('path')
 
 module.exports = {
