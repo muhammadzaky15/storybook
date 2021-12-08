c94f8c4c4fe73be00ab084bcd6fb4cbb2dc17e68
m.zaky <m.zaky@hukumonline.com>
2021-12-08 15:44:52 +0700

    fix package storybook and tailwind and fix post css

--- src/stories/Text.jsx
+++ src/stories/Text.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Text.css";
 
 export const Text = () => (
     <p className="text-9xl">Tes Text Tailwind</p>
--- src/stories/Text.stories.jsx
+++ src/stories/Text.stories.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "../index.css"
-
 import { Text } from "./Text";
 
 export default {
