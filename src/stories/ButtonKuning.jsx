f6cdb8bb24fd79468d5d5e76bdb7d8b959e7f22e
Rian nazar <riannazar03@gmail.com>
2022-01-06 22:44:08 +0700

    storybook

---
+++ src/stories/ButtonKuning.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+import "./ButtonKuning.css";
+const ButtonKuning = ()=>(
+    <button class="w-56 md:w-40 h-16 md:h-10 rounded-lg  bg-yellow-400 font-bold  ">BERLANGGANAN</button>
+)
+
+export default ButtonKuning
---
+++ src/stories/ButtonKuning.stories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ButtonKuning from "./ButtonKuning";
+
+export default {
+    title: "Example/Button Kuning",
+    component: ButtonKuning
+}
+
+const Template = (args) => <ButtonKuning {...args} />
+
+export const Default = Template.bind({})
